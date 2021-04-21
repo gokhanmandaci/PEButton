9f5eca985a5339a408a7dfbda4557bef292e7485
Gökhan Mandacı <gokhanmandaci@gmail.com>
2021-04-21 20:59:02 +0300

    source file updated

--- Source/PEAnimationOptions.swift
+++ Source/PEAnimationOptions.swift
@@ -8,12 +8,20 @@
 import UIKit
 
 struct PEAnimationOptions {
+    /// Delays down animation if necessary. Default 0
     var touchDownDelay: TimeInterval = 0
+    /// Down spring with damping value. Default 1.0
     var touchDownUsingSpringWithDamping: CGFloat = 1.0
+    /// Down initial spring velocity value. Default 1.0
     var touchDownInitialSpringVelocity: CGFloat = 1.0
-    var touchDownOptions: UIView.AnimationOptions = .curveLinear
+    /// Down animation option. Default .curveEaseInOut
+    var touchDownOptions: UIView.AnimationOptions = .curveEaseInOut
+    /// Delays up animation if necessary. Default 0
     var touchUpDelay: TimeInterval = 0
+    /// Up spring with damping value. Default 1.0
     var touchUpUsingSpringWithDamping: CGFloat = 1.0
+    /// Up initial spring velocity value. Default 1.0
     var touchUpInitialSpringVelocity: CGFloat = 1.0
-    var touchUpOptions: UIView.AnimationOptions = .curveLinear
+    /// Up animation option. Default .curveEaseInOut
+    var touchUpOptions: UIView.AnimationOptions = .curveEaseInOut
 }
